a718527f341bcd2396c72d331698ff69537caab5
Woad Stoat <woadstoat@gmail.com>
2025-12-02 10:01:45 +0000

    !U Updated assembly

--- obj/Debug/net8.0/Pong.AssemblyInfo.cs
+++ obj/Debug/net8.0/Pong.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pong")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3aa068e2aa8c91d38e010c98298bb8fda561397")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb46d5156161f0100a1cfbe474842f7898d0df37")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pong")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pong")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
